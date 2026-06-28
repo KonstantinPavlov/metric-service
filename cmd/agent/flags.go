496ea160cedf236a4b67b4547deb73ebe902dbf1
Pavlov Konstantin <Pavlov.K.Gen@yandex.ru>
2026-06-28 16:23:56 +0000

    Increment4. Fix default address for htt server.

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -7,7 +7,7 @@ var flagReportInterval int
 var flagPollInterval int
 
 func parseFlags() {
-	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.IntVar(&flagReportInterval, "r", 10, "report interval in seconds")
 	flag.IntVar(&flagPollInterval, "p", 2, "poll interval in seconds")
 	flag.Parse()
